1a2ecefe97a441826ebc9e4051e4d823428bd5cb
User 616 <you@example.com>
2026-08-06 05:46:48 +0500

    fix: remove electron-is-dev and use app.isPackaged to prevent false dev mode in production

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,6 +1,5 @@
 const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const path = require('path');
-const isDev = require('electron-is-dev');
 const fs = require('fs');
 
 function createWindow() {
@@ -22,6 +21,8 @@ function createWindow() {
     },
     backgroundColor: '#0d1117'
   });
+
+  const isDev = !app.isPackaged;
 
   if (isDev) {
     mainWindow.loadURL('http://127.0.0.1:5173');
